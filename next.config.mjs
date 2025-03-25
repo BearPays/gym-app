272d4c50c3ca50d256a59e85baf58873fe98ce6a
BearPays <bjorn.borgar@gmail.com>
2025-03-25 16:27:14 +0100

    feat: refactor PWA configuration and add service worker for caching strategies

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
-import withPWA from 'next-pwa';
+/** @type {import('next').NextConfig} */
+import withPWAInit from 'next-pwa';
 
-const isProd = process.env.NODE_ENV === 'production';
+// Initialize the withPWA function
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV !== 'production',
+  register: true,
+  skipWaiting: true,
+});
 
-const nextConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd, // Disable PWA in development mode
-    register: true,
-    skipWaiting: true,
-  },
+// Define Next.js config separately
+const nextConfig = {
   images: {
     domains: [
       'images.unsplash.com',
@@ -17,6 +19,7 @@ const nextConfig = withPWA({
       'cdn.muscleandstrength.com',
     ],
   },
-});
+};
 
-export default nextConfig;
+// Apply the PWA wrapper to the Next.js config
+export default withPWA(nextConfig);
